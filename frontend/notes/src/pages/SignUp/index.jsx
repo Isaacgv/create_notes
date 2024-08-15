b605e252fa98cf93031ccdd9484dba934901c6bb
Isaac <isaacgonzalesv@gmail.com>
2024-08-15 10:31:47 +0200

    add register cors and user register

--- frontend/notes/src/pages/SignUp/index.jsx
+++ frontend/notes/src/pages/SignUp/index.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { Container, Form, Background } from './styles';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import {  FiMail, FiLock, FiUser} from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
+import { api } from "../../seervices/api"; 
+
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp(){
+    if(!name || !email || !password) {
+      return alert("Fill in all the fields")
+    }
+
+    api.post("/users", { name, email, password})
+
+  }
+
   return (
     <Container>
       <Background />
@@ -17,18 +33,21 @@ export function SignUp() {
             placeholder="Name"
             type="text"
             icon={FiUser}
+            onChange={e => setName(e.target.value)}
         />
         <Input 
             placeholder="Email"
             type="text"
             icon={FiMail}
+            onChange={e => setEmail(e.target.value)}
         />
         <Input 
             placeholder="Password"
             type="password"
             icon={FiLock}
+            onChange={e => setPassword(e.target.value)}
         />
-        <Button title="Register" />
+        <Button title="Register" onClick={handleSignUp}/>
 
         <Link to="/">Login</Link>
             
