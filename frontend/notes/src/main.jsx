85417e8bc7ef55b485e1b080ed81d87958be1df6
Isaac <isaacgonzalesv@gmail.com>
2024-08-15 11:18:26 +0200

    add hooks

--- frontend/notes/src/main.jsx
+++ frontend/notes/src/main.jsx
@@ -6,6 +6,8 @@ import GlobalStyle  from './styles/global'
 
 import theme from './styles/theme'
 
+import { AuthProvider } from './hooks/auth'
+
 import { Routes } from './routes'
 
 
@@ -13,7 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
--- frontend/notes/src/pages/SignUp/index.jsx
+++ frontend/notes/src/pages/SignUp/index.jsx
@@ -3,7 +3,7 @@ import { Container, Form, Background } from './styles';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import {  FiMail, FiLock, FiUser} from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { api } from "../../seervices/api"; 
 
@@ -12,12 +12,25 @@ export function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+
   function handleSignUp(){
     if(!name || !email || !password) {
       return alert("Fill in all the fields")
     }
 
     api.post("/users", { name, email, password})
+    .then(() => {
+      alert("User register!");
+      navigate("/");
+    })
+    .catch(error => {
+      if(error.response){
+        alert(error.response.data.message);
+      }else {
+        alert("Unable to register")
+      }
+    })
 
   }
 
