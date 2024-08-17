f23869bfe85f58e99904d6b2db9ab7f997b6808e
Isaac <isaacgonzalesv@gmail.com>
2024-08-17 17:57:20 +0200

    update storage

--- frontend/notes/src/hooks/auth.jsx
+++ frontend/notes/src/hooks/auth.jsx
@@ -1,10 +1,48 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { api } from '../services/api';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+    const [data, setData] = useState({});
+
+    async function singIn({ email, password }) {
+
+        try{
+            const response = await api.post("/sessions", { email, password });
+            const { user, token } = response.data;
+
+            localStorage.setItem("@notes:user", JSON.stringify(user));
+            localStorage.setItem("@notes:token", token);
+
+            api.defaults.headers.authorization = `Beare ${token}`;
+            setData({ user, token });
+            console.log(response);
+        } catch (error){
+            if (error.response) {
+                alert(error.response.data.mpessage);
+            } else {
+                alert("Impossivel to enter");
+            }
+        }
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@notes:token");
+        const user = localStorage.getItem("@notes:user");
+
+        if (token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+
+    }, [])
     return (
-        <AuthContext.Provider value={{name: 'Juan', email:'@test.com'}}>
+        <AuthContext.Provider value={{ singIn, user: data.user }}>
             {children}
         </AuthContext.Provider>
     )
